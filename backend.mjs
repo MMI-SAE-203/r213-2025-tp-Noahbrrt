abc6aa79a94604e675dd48f566368faf4a78f8d3
Noahbrrt <noah.barret@edu.univ-fcomte.fr>
2025-01-31 11:42:16 +0100

    Images

---
+++ backend.mjs
@@ -0,0 +1,48 @@
+import PocketBase from 'pocketbase' ;
+const pb = new PocketBase('http://127.0.0.1:8090') ;
+
+
+
+export async function allMaisons() {
+let records = await pb.collection('maisons').getFullList() ;
+
+records = records.map((maison) => {
+    maison.imgUrl = pb.files.getURL(maison, maison.images);
+    return maison;
+});
+
+return records ;
+}
+
+
+/*
+export async function OneID(id) {
+const oneRecords = await pb.collection('maisons').getOne(id) ;
+return oneRecords ;
+}
+
+
+export async function allMaisonsFavoris() {
+const Favoris = await pb.collection('maisons').getFullList({ filter: 'favori = true'
+    }) ;
+return Favoris ;
+}
+
+export async function allMaisonsSorted() {
+const Prix = await pb.collection('maisons').getFullList({ sort: 'prix'}
+    ) ;
+return Prix ;
+}
+
+export async function bySurface(s) {
+const Surface = await pb.collection('maisons').getFullList({ filter: `surface > ${s}`,}
+    ) ;
+return Surface ;
+}
+
+export async function surfaceORprice(s,p) {
+const SurfaceORPrix = await pb.collection('maisons').getFullList({ filter: `surface > ${s}&& prix < ${p}` }
+    ) ;
+return SurfaceORPrix ;
+}
+*/
